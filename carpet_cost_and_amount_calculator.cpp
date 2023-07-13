befef5d3b9d4c15bb84d05b40cd0c403c78141b7
Faris Khan <98594863+f314s@users.noreply.github.com>
2023-07-13 22:07:36 +0600

    Update carpet_cost_and_amount_calculator.cpp
    code improved

--- carpet_cost_and_amount_calculator.cpp
+++ carpet_cost_and_amount_calculator.cpp
@@ -7,28 +7,28 @@ int main()
     char repeat_calculation = 'y';
 
 while (repeat_calculation == 'y' || repeat_calculation == 'Y'){
-    double length_of_room, width_of_room, length_of_carpet, amount_of_carpet, width_of_carpet, cost, total_cost, area_of_room, area_of_carpet;
+    double room_length, room_width, carpet_length, carpet_width, width_of_carpet, carpet_cost, total_carpet_cost, room_area, carpet_area, carpet_amount;
 
     cout << "enter the length of room:" << endl;
-    cin >> length_of_room;
+    cin >> room_length;
     cout << "enter the width of room:" << endl;
-    cin >> width_of_room;
+    cin >> room_width;
     cout << "enter the length of carpet:" << endl;
-    cin >> length_of_carpet;
+    cin >> carpet_length;
     cout << "enter the width of carpet:" << endl;
-    cin >> width_of_carpet;
+    cin >> carpet_width;
     cout << "enter the cost of carpet:" << endl;
-    cin >> cost;
+    cin >> carpet_cost;
 
-    area_of_room = length_of_room * width_of_room;
-    area_of_carpet = length_of_carpet * width_of_carpet;
+    room_area = room_length * room_width;
+    carpet_area = carpet_length * carpet_width;
 
-    amount_of_carpet = (area_of_room / area_of_carpet);
-    total_cost =  amount_of_carpet * cost;
+    carpet_amount = (room_area / carpet_area);
+    total_carpet_cost =  carpet_amount * carpet_cost;
 
 
-    cout << "total carpet needed is : " << amount_of_carpet << endl;s
-    cout << "your total cost is : " << total_cost << endl;
+    cout << "total carpet needed is : " << carpet_amount << endl;
+    cout << "your total cost is : " << total_carpet_cost << endl;
     cout << "calculate again y/n" << endl;
     cin >> repeat_calculation;
 }
